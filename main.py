a1ac837454bc3bd6f868744e012686642295f125
BenFaruna <benjaminfaruna@gmail.com>
2023-09-07 06:02:14 +0100

    API endpoint has been created with some fields returning nil

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+import calendar
+from datetime import datetime
+
+import pytz
+
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/api", methods=["GET"])
+def api_route():
+    query = request.args
+    slack_name = query.get("slack_name")
+    track = query.get("track")
+    current_time = datetime.now(tz=pytz.utc)
+    current_day = current_time.strftime("%Y-%M-%dT%H:%M:%SZ") #.isoformat(timespec="seconds")# + "Z"
+
+    return jsonify({
+        "slack_name": slack_name,
+        "current_day": calendar.day_name[current_time.weekday()],
+        "utc_time": current_day,
+        "track": track,
+        "github_file_url": None,
+        "github_file_repo": os.getenv("github_file_repo"), # environmental variable
+        "status_code": 200
+    })
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
